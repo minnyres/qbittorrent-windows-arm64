cf78c35cf029b514ba5c6b33587214ef0bcc1c75
minnyres <liuthgw23@gmail.com>
2022-11-28 19:59:48 +0800

    Update build.sh
    1. use libtorrent 2.0.x
    2. upgrade to qbittorrent 4.5.0

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@
 
 set -e
 runtime=clang-aarch64
-libtorrent_ver=1.2.17
-qbittorrent_ver=4.4.5
+libtorrent_ver=2.0.8
+qbittorrent_ver=4.5.0
 
 workdir=$PWD
 
-pacman -S --needed diffutils p7zip mingw-w64-${runtime}-boost mingw-w64-${runtime}-clang mingw-w64-${runtime}-cmake mingw-w64-${runtime}-qt6-base mingw-w64-${runtime}-qt6-svg mingw-w64-${runtime}-qt6-tools mingw-w64-${runtime}-qt6-translations
+pacman -S --needed patch diffutils p7zip mingw-w64-${runtime}-boost mingw-w64-${runtime}-clang mingw-w64-${runtime}-cmake mingw-w64-${runtime}-qt6-base mingw-w64-${runtime}-qt6-svg mingw-w64-${runtime}-qt6-tools mingw-w64-${runtime}-qt6-translations
 
 # Build libtorrent
 wget -nc https://github.com/arvidn/libtorrent/releases/download/v${libtorrent_ver}/libtorrent-rasterbar-${libtorrent_ver}.tar.gz
 tar -xf libtorrent-rasterbar-${libtorrent_ver}.tar.gz
 cd libtorrent-rasterbar-${libtorrent_ver}
-# patch -p1 < ${workdir}/0001-fix-stat-marco-conflict.patch
+patch -p1 < ${workdir}/0001-fix-stat-marco-conflict.patch
 mkdir -p build
 cd build 
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX="C:/libtorrent" ..
@@ -27,16 +27,16 @@ tar -xf qbittorrent-${qbittorrent_ver}.tar.xz
 cd qbittorrent-${qbittorrent_ver}
 mkdir -p build
 cd build 
-export PKG_CONFIG_PATH="/c/libtorrent/lib/pkgconfig"
 export LDFLAGS="-lws2_32"
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH="C:/libtorrent/share/cmake/Modules" -DCMAKE_INSTALL_PREFIX="C:/qbittorrent" -DSTACKTRACE=OFF -DQT6=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="C:/qbittorrent/" -DCMAKE_PREFIX_PATH="C:/libtorrent/" -DCMAKE_MODULE_PATH="C:/libtorrent/share/cmake/Modules" -DSTACKTRACE=OFF -DQT6=ON ..
 cmake --build .
+cmake --install .
 
 # Package
 cd $workdir
 mkdir -p qbittorrent
 cd qbittorrent
-cp ../qbittorrent-${qbittorrent_ver}/build/qbittorrent.exe .
+cp /c/qbittorrent/bin/* .
 windeployqt-qt6 qbittorrent.exe
 cp /clangarm64/bin/zlib1.dll .
 cp /clangarm64/bin/libpng16-16.dll .
@@ -65,3 +65,7 @@ cp /clangarm64/bin/libb2-1.dll .
 cp /c/libtorrent/bin/libtorrent-rasterbar.dll .
 cd ..
 7z a -mx9 qbittorrent_${qbittorrent_ver}_arm64.7z qbittorrent
+
+# clean up
+rm -rf /c/libtorrent
+rm -rf /c/qbittorrent
